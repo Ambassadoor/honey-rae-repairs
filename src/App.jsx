0738eeaa3dfafd0abff9ac31f6796cd6e44143d4
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-10-13 20:01:03 -0500

    Added tickets

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getAllTickets } from "./services/ticketService.js"
+import "./App.css"
 
 export const App = () => {
-const handleButtonClick = () => {
-  setClicked(clicked + 1)
-}
+  const [allTickets, setAllTickets] =  useState([])
+  const [showEmergency, setShowEmergency] = useState(false)
+  const [displayedTickets, setDisplayedTickets] = useState([])
 
-const [clicked, setClicked] = useState(0)
+  useEffect(() => {
+    getAllTickets().then(tickets => {
+      setAllTickets(tickets)
+    })
+  }, [])
+
+  useEffect(() => {
+    setDisplayedTickets(allTickets.filter(ticket => showEmergency ? ticket.emergency : true))
+  }, [showEmergency, allTickets])
 
 
   return (
     <>
-      <h1>Hello!</h1>
-      <div>This is amazing!</div>
-      <button onClick={handleButtonClick} className="btn-secondary">Click me!</button>
-      <div>Count: {clicked}</div>
+      <div className="tickets-container">
+        <h2>Tickets</h2>
+        <div>
+          <button className="filter-btn btn-primary" onClick={() => {setShowEmergency(true)}}> Emergency</button>
+          <button className="filter-btn btn-info" onClick={() => {setShowEmergency(false)}}>Show All</button>
+        </div>
+        <article className="tickets">
+          {displayedTickets.map(ticket => {
+            return (
+              <section className="ticket" key={ticket.id}>
+                <header className="ticket-info">#{ticket.id}</header>
+                <div>{ticket.description}</div>
+                <footer>
+                  <div>
+                    <div className="ticket-info">emergency</div>
+                    <div>{ticket.emergency ? "yes" : "no"}</div>
+                  </div>
+                </footer>
+              </section>
+            )
+          })}
+        </article>
+      </div>
     </>
   )
 }
